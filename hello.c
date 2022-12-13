3090e44a3bf8dbb990dc6c2dcae25b79374c413a
Obojo <kingvictor009@gmail.com>
2022-12-13 02:37:39 +0100

    Hello there!

--- hello.c
+++ hello.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print "hello world!"
+ * main - greetings to the world
  *
  * Return: Always 0 (Successful)
  */
 int main(void)
 {
 	printf("Hello World!\n");
-	printf("Howdy, neighbor! This is my C program.\n");
+	printf("I am Victor, you are welcome to my C journey.\n");
 	return (0);
 }
